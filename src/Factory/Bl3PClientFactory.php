8d5e657bc19a55fb3564cf04be79599fbe6b6526
Jorijn Schrijvershof <jorijn@jorijn.com>
2020-04-25 08:33:38 +0200

    created test for api factory

--- src/Factory/Bl3PClientFactory.php
+++ src/Factory/Bl3PClientFactory.php
@@ -4,12 +4,18 @@ declare(strict_types=1);
 
 namespace Jorijn\Bl3pDca\Factory;
 
+use InvalidArgumentException;
 use Jorijn\Bl3pDca\Client\Bl3PClient;
+use Jorijn\Bl3pDca\Client\Bl3PClientInterface;
 
 class Bl3PClientFactory
 {
-    public function createApi(): Bl3PClient
+    public function createApi(): Bl3PClientInterface
     {
+        if (!isset($_ENV['BL3P_API_URL'], $_ENV['BL3P_PUBLIC_KEY'], $_ENV['BL3P_PRIVATE_KEY'])) {
+            throw new InvalidArgumentException('Incomplete configuration, missing BL3P_API_URL, BL3P_PUBLIC_KEY or BL3P_PRIVATE_KEY');
+        }
+
         return new Bl3PClient($_ENV['BL3P_API_URL'], $_ENV['BL3P_PUBLIC_KEY'], $_ENV['BL3P_PRIVATE_KEY']);
     }
 }
---
+++ tests/Factory/Bl3PClientFactoryTest.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Jorijn\Bl3pDca\Factory;
+
+use InvalidArgumentException;
+use Jorijn\Bl3pDca\Client\Bl3PClientInterface;
+use Jorijn\Bl3pDca\Factory\Bl3PClientFactory;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @coversDefaultClass \Jorijn\Bl3pDca\Factory\Bl3PClientFactory
+ */
+class Bl3PClientFactoryTest extends TestCase
+{
+    /**
+     * @covers ::createApi
+     */
+    public function testCreateApiWithoutConfigurationPresent(): void
+    {
+        $factory = new Bl3PClientFactory();
+
+        $this->expectException(InvalidArgumentException::class);
+
+        $factory->createApi();
+    }
+
+    /**
+     * @covers ::createApi
+     */
+    public function testCreateApi(): void
+    {
+        $factory = new Bl3PClientFactory();
+
+        $_ENV['BL3P_API_URL'] = 'url'.mt_rand();
+        $_ENV['BL3P_PUBLIC_KEY'] = 'pub_key'.mt_rand();
+        $_ENV['BL3P_PRIVATE_KEY'] = 'private_key'.mt_rand();
+
+        self::assertInstanceOf(Bl3PClientInterface::class, $factory->createApi());
+    }
+}
