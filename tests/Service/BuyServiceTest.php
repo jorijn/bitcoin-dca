790bdcc60319a158be12c45f5d11bd5d3055a6be
Jorijn Schrijvershof <jorijn@jorijn.com>
2020-05-05 09:24:48 +0200

    finished test for BuyService

--- tests/Service/BuyServiceTest.php
+++ tests/Service/BuyServiceTest.php
@@ -5,6 +5,8 @@ declare(strict_types=1);
 namespace Tests\Jorijn\Bl3pDca\Service;
 
 use Jorijn\Bl3pDca\Client\Bl3pClientInterface;
+use Jorijn\Bl3pDca\Event\BuySuccessEvent;
+use Jorijn\Bl3pDca\Exception\BuyTimeoutException;
 use Jorijn\Bl3pDca\Service\BuyService;
 use PHPUnit\Framework\MockObject\MockObject;
 use PHPUnit\Framework\TestCase;
@@ -13,6 +15,7 @@ use Psr\Log\LoggerInterface;
 
 /**
  * @coversDefaultClass \Jorijn\Bl3pDca\Service\BuyService
+ * @covers ::__construct
  *
  * @internal
  */
@@ -24,7 +27,6 @@ final class BuyServiceTest extends TestCase
     private $logger;
     /** @var EventDispatcherInterface|MockObject */
     private $dispatcher;
-    private int $timeout;
     private BuyService $service;
 
     protected function setUp(): void
@@ -34,25 +36,214 @@ final class BuyServiceTest extends TestCase
         $this->client = $this->createMock(Bl3pClientInterface::class);
         $this->logger = $this->createMock(LoggerInterface::class);
         $this->dispatcher = $this->createMock(EventDispatcherInterface::class);
-        $this->timeout = 2;
 
         $this->service = new BuyService(
             $this->client,
             $this->dispatcher,
             $this->logger,
-            $this->timeout
+            5
         );
     }
 
-    public function testBuyFillDelayedButWithinTimeout(): void
+    public function providerOfBuyScenarios(): array
     {
+        return [
+            'buy fills immediately' => [0, false],
+            'buy fills after three seconds' => [3, false],
+            'buy fills after seven seconds, but timeout is set at 5 -> timeout + cancellation' => [7, true],
+            'buy fills immediately but fees settled in EUR' => [0, false, 'EUR'],
+        ];
     }
 
-    public function testBuyCannotBeFilledWithinTimeout(): void
-    {
+    /**
+     * @covers ::buy
+     * @dataProvider providerOfBuyScenarios
+     *
+     * @throws \Exception
+     */
+    public function testBuyingWithVariousOptions(
+        int $fillDelayed = 0,
+        bool $expectCancellation = false,
+        string $feeCurrency = 'BTC'
+    ): void {
+        $amount = random_int(5, 10);
+        $buyParams = $this->createBuy($amount);
+        $orderId = random_int(1000, 2000);
+        $tag = 't'.random_int(1000, 2000);
+
+        $buyResult = $this->getNewOrderResult($orderId);
+        $delayedResult = $this->getDelayedOrderResult();
+
+        [
+            $amountBought,
+            $amountBoughtDisplayed,
+            $feesDisplayed,
+            $feeSpent,
+            $totalSpentDisplayed,
+            $averageCostDisplayed,
+            $closedResult,
+        ] = $this->getClosedOrderResult($feeCurrency);
+
+        $startingTime = time();
+        $orderClosedAt = $startingTime + $fillDelayed;
+        $attemptedBuy = $attemptedDelayedCall = $attemptedResultCall = $attemptedCancellation = false;
+
+        $this->client
+            ->expects(static::atLeastOnce())
+            ->method('apiCall')
+            ->willReturnCallback(function (string $url, array $parameters) use (
+                $orderId,
+                $buyParams,
+                $closedResult,
+                $delayedResult,
+                $orderClosedAt,
+                $buyResult,
+                &$attemptedBuy,
+                &$attemptedDelayedCall,
+                &$attemptedResultCall,
+                &$attemptedCancellation
+            ) {
+                switch ($url) {
+                    case 'BTCEUR/money/order/add':
+                        $attemptedBuy = true;
+
+                        self::assertArrayHasKey('type', $parameters);
+                        self::assertSame($buyParams['type'], $parameters['type']);
+                        self::assertArrayHasKey('amount_funds_int', $parameters);
+                        self::assertSame($buyParams['amount_funds_int'], $parameters['amount_funds_int']);
+                        self::assertArrayHasKey('fee_currency', $parameters);
+                        self::assertSame($buyParams['fee_currency'], $parameters['fee_currency']);
+
+                        return $buyResult;
+                    case 'BTCEUR/money/order/result':
+                        self::assertArrayHasKey(BuyService::ORDER_ID, $parameters);
+                        self::assertSame($orderId, $parameters[BuyService::ORDER_ID]);
+
+                        if (time() < $orderClosedAt) {
+                            $attemptedDelayedCall = true;
+
+                            return $delayedResult;
+                        }
+
+                        $attemptedResultCall = true;
+
+                        return $closedResult;
+                    case 'BTCEUR/money/order/cancel':
+                        $attemptedCancellation = true;
+
+                        self::assertArrayHasKey(BuyService::ORDER_ID, $parameters);
+                        self::assertSame($orderId, $parameters[BuyService::ORDER_ID]);
+
+                        return []; // if the code should ever act on cancellation result, this would be the place to add
+                    default:
+                        $this->addToAssertionCount(1);
+
+                        throw new \Exception('did not expect call to location '.$url);
+                }
+            })
+        ;
+
+        $returnedBuyOrder = null;
+        $this->dispatcher
+            ->expects($expectCancellation ? static::never() : static::once())
+            ->method('dispatch')
+            ->with(static::callback(static function (BuySuccessEvent $event) use ($tag, &$returnedBuyOrder) {
+                self::assertSame($tag, $event->getTag());
+                $returnedBuyOrder = $event->getBuyOrder();
+
+                return true;
+            }))
+        ;
+
+        if ($expectCancellation) {
+            $this->expectException(BuyTimeoutException::class);
+        }
+
+        $completedBuyOrder = $this->service->buy($amount, $tag);
+
+        static::assertTrue($attemptedBuy);
+        static::assertSame($fillDelayed > 0, $attemptedDelayedCall);
+        static::assertSame(!$expectCancellation, $attemptedResultCall);
+        static::assertSame($expectCancellation, $attemptedCancellation);
+
+        if (!$expectCancellation) {
+            static::assertSame($returnedBuyOrder, $completedBuyOrder);
+            static::assertSame($amountBought, $completedBuyOrder->getAmountInSatoshis());
+            static::assertSame('BTC' === $feeCurrency ? $feeSpent : 0, $completedBuyOrder->getFeesInSatoshis());
+            static::assertSame($amountBoughtDisplayed, $completedBuyOrder->getDisplayAmountBought());
+            static::assertSame($totalSpentDisplayed, $completedBuyOrder->getDisplayAmountSpent());
+            static::assertSame($averageCostDisplayed, $completedBuyOrder->getDisplayAveragePrice());
+            static::assertSame($feesDisplayed, $completedBuyOrder->getDisplayFeesSpent());
+        }
     }
 
-    public function testBuyIsFilledImmediately(): void
+    protected function createBuy(int $amount): array
     {
+        return [
+            'type' => 'bid',
+            'amount_funds_int' => $amount * 100000,
+            'fee_currency' => 'BTC',
+        ];
+    }
+
+    /**
+     * @throws \Exception
+     */
+    protected function getClosedOrderResult(string $feeCurrency): array
+    {
+        $closedResult = [
+            BuyService::DATA => [
+                BuyService::STATUS => BuyService::ORDER_STATUS_CLOSED,
+                BuyService::TOTAL_AMOUNT => [
+                    BuyService::VALUE_INT => $amountBought = random_int(1000, 2000),
+                    BuyService::DISPLAY => $amountBoughtDisplayed = $amountBought.' BTC',
+                ],
+                BuyService::TOTAL_FEE => [
+                    BuyService::CURRENCY => $feeCurrency,
+                    BuyService::DISPLAY => $feesDisplayed = random_int(1000, 2000).' '.$feeCurrency,
+                    BuyService::VALUE_INT => $feeSpent = random_int(1000, 2000),
+                ],
+                BuyService::TOTAL_SPENT => [
+                    BuyService::DISPLAY_SHORT => $totalSpentDisplayed = random_int(1000, 2000).' EUR',
+                ],
+                BuyService::AVG_COST => [
+                    BuyService::DISPLAY_SHORT => $averageCostDisplayed = random_int(1000, 2000).' EUR',
+                ],
+            ],
+        ];
+
+        return [
+            $amountBought,
+            $amountBoughtDisplayed,
+            $feesDisplayed,
+            $feeSpent,
+            $totalSpentDisplayed,
+            $averageCostDisplayed,
+            $closedResult,
+        ];
+    }
+
+    /**
+     * @return \string[][]
+     */
+    protected function getDelayedOrderResult(): array
+    {
+        return [
+            BuyService::DATA => [
+                BuyService::STATUS => 'open',
+            ],
+        ];
+    }
+
+    /**
+     * @return \int[][]
+     */
+    protected function getNewOrderResult(int $orderId): array
+    {
+        return [
+            BuyService::DATA => [
+                BuyService::ORDER_ID => $orderId,
+            ],
+        ];
     }
 }
